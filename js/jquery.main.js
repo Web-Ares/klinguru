581c49a39dee560487744b6140be2f49c46e83f7
alex <alexnikol@e-mail.ua>
2016-01-16 13:49:28 +0200

    f

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    $(".step-item__selects label").click(function(event){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).find("input").attr('checked',false);
+        }else{
+            $(this).addClass('active');
+            $(this).find("input").attr('checked',true);
+        }
+        event.stopPropagation();
+        event.cancelBubble = true;
+
+        return false;
+    })
 } );
 
 var Shablon = function (obj) {
